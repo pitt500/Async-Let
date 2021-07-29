2bc61ead7916d213d982d894e3fb38392f650fae
pitt500 <>
2021-07-29 07:31:17 -0700

    Add starter project

---
+++ AsyncLet/AsyncLet/AsyncLetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncLetApp.swift
+//  AsyncLet
+//
+//  Created by Pedro Rojas on 29/07/21.
+//
+
+import SwiftUI
+
+@main
+struct AsyncLetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncLet/AsyncLet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AsyncLet
+//
+//  Created by Pedro Rojas on 29/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
