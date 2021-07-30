d45d7d7fd33ff0caf823730d15bed7c6783d6bf5
pitt500 <>
2021-07-30 12:08:13 -0700

    Add async let demo with pokemon images

--- AsyncLet/AsyncLet/ContentView.swift
+++ AsyncLet/AsyncLet/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var loader = PokemonLoader()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            ZStack {
+                VStack {
+                    ForEach(loader.pokemonData, id: \.hashValue) { data in
+                        Image(data: data)
+                            .resizable()
+                            .frame(width: 120,height: 120)
+                            .aspectRatio(contentMode: .fit)
+                    }
+                }
+
+                if loader.isLoading {
+                    ProgressView()
+                }
+            }
+        }
+        .task {
+            await loader.load()
+        }
+    }
+}
+
+extension Image {
+    init(data: Data) {
+        if let uiImage = UIImage(data: data) {
+            self.init(uiImage: uiImage)
+        } else {
+            self.init(systemName: "questionmark")
+        }
     }
 }
 
---
+++ AsyncLet/AsyncLet/PokemonLoader.swift
@@ -0,0 +1,94 @@
+//
+//  PokemonLoader.swift
+//  PokemonLoader
+//
+//  Created by Pedro Rojas on 29/07/21.
+//
+
+import Foundation
+
+enum PokemonError: Error, Identifiable {
+    case serverError
+    case noData
+
+    var id: Self {
+        self
+    }
+}
+
+class PokemonLoader: ObservableObject {
+    @Published var error = false
+    @Published var pokemonData: [Data] = []
+    @Published var isLoading = false
+
+    func getImage(pokemonId: Int) async throws -> Data {
+        let url = URL(string: "https://pokeres.bastionbot.org/images/pokemon/\(pokemonId).png")!
+        let (data, response) = try await URLSession.shared.data(from: url)
+
+        guard (response as? HTTPURLResponse)?.statusCode == 200
+        else { throw PokemonError.serverError }
+
+        return data
+    }
+
+    func load() async {
+        /// Comparte the time between these two methods!
+        //await loadWithAwait()
+        await loadWithAsyncLet()
+    }
+
+    @MainActor
+    func loadWithAwait() async {
+        do {
+            let start = Date.now
+            isLoading = true
+            let bulbasaur = try await getImage(pokemonId: 1)
+            let charizard = try await getImage(pokemonId: 6)
+            let squirtle  = try await getImage(pokemonId: 7)
+            let pidgeotto = try await getImage(pokemonId: 17)
+            let kingler   = try await getImage(pokemonId: 99)
+            let pikachu   = try await getImage(pokemonId: 25)
+            calculateTime(from: start)
+
+
+
+            pokemonData = [bulbasaur, charizard, squirtle, pidgeotto, kingler,pikachu]
+            isLoading = false
+        } catch {
+            print(error)
+            self.error = true
+        }
+    }
+
+    @MainActor
+    func loadWithAsyncLet() async {
+        do {
+            let start = Date.now
+            isLoading = true
+            async let bulbasaur = getImage(pokemonId: 1)
+            async let charizard = getImage(pokemonId: 6)
+            async let squirtle  = getImage(pokemonId: 7)
+            async let pidgeotto = getImage(pokemonId: 17)
+            async let kingler   = getImage(pokemonId: 99)
+            async let pikachu   = getImage(pokemonId: 25)
+            calculateTime(from: start)
+
+
+            pokemonData = try await [bulbasaur, charizard, squirtle, pidgeotto, kingler,pikachu]
+            isLoading = false
+
+        } catch {
+            print(error)
+            self.error = true
+        }
+    }
+
+    func calculateTime(from start: Date) {
+        let end = Date.now
+        let diffs = Calendar.current.dateComponents([.nanosecond], from: start, to: end)
+        let nanoseconds = diffs.nanosecond!
+        let seconds = Double(nanoseconds)/1_000_000_000.0
+        print("Total time: \(seconds) seconds")
+        print("(\(nanoseconds) nanoseconds)")
+    }
+}
